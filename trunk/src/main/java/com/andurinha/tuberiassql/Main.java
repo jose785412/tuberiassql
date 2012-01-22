4276b5fad6c90b18811dfa5e394cd7dd64b0b5df
bris2376 <bris2376@2d7a32ef-5d65-e2a8-58be-7d95d14da3e2>
2012-01-22 02:52:10 +0000

    Issue 1: Crear Estructura del Proyecto

---
+++ trunk/src/main/java/com/andurinha/tuberiassql/Main.java
@@ -0,0 +1,50 @@
+/**
+ 	Tuberias SQL : Software de terminal para mover datos entre bases de datos
+    Copyright (C) 2012  bris2376@gmail.com
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ 
+ **/
+package src.main.java.com.andurinha.tuberiassql;
+
+/**
+ * @author andurinha
+ *
+ */
+public class Main {
+
+	/**
+		<PRE>
+		Proyecto    : Tuberias SQL
+		Codigo	    : tuberiassql 
+		ID_TAREA    : Issue 1: Crear Estructura del Proyecto
+		Metodo      : main
+		Descripcion :
+	
+		Punto de entrada de la aplicacion
+	
+	    Fecha       : 22/01/2012
+		Autor       : bris2376@gmail.com
+		</PRE>
+	
+	void
+	@param args
+	
+	 **/
+	public static void main(String[] args) {
+		
+
+	}
+
+}
